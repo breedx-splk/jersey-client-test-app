b790ffb03c09e3ed7f1363174ff868885d12f93f
Jason Plumb <jplumb@splunk.com>
2021-01-04 17:19:08 -0800

    project setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,5 +10,9 @@ repositories {
 }
 
 dependencies {
-    testCompile("junit", "junit", "4.12")
+    implementation("org.glassfish.jersey.core:jersey-client:2.33")
+    implementation("org.glassfish.jersey.inject:jersey-hk2:2.33")
+    implementation("org.glassfish.jersey.media:jersey-media-json-jackson:2.33")
+
+    testImplementation("junit", "junit", "4.12")
 }
--- src/main/java/com/splunk/Main.java
+++ src/main/java/com/splunk/Main.java
@@ -1,6 +1,44 @@
 package com.splunk;
 
+import org.glassfish.jersey.client.ClientConfig;
+import org.glassfish.jersey.jackson.JacksonFeature;
+import org.glassfish.jersey.logging.LoggingFeature;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.MediaType;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 public class Main {
 
+    private final Client client;
+
+    public static void main(String[] args) {
+        var executor = Executors.newSingleThreadScheduledExecutor();
+        var client = ClientBuilder.newClient(new ClientConfig()
+                .register(LoggingFeature.class)
+                .register(JacksonFeature.class)
+        );
+        var worker = new Main(client);
+        executor.scheduleAtFixedRate(worker::doWork, 0, 1, TimeUnit.SECONDS);
+    }
+
+    public Main(Client client) {
+        this.client = client;
+    }
+
+    void doWork() {
+        try {
+            System.out.println("Let's fetch a snack...");
+            var webTarget = client.target("http://localhost:8000/").path("snacks/jerky.json");
+            var invocationBuilder = webTarget.request(MediaType.APPLICATION_JSON);
+            var response = invocationBuilder.get();
+            var snack = response.readEntity(Snack.class);
+            System.out.println("Fetched a delicious snack: " + snack);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
 }
